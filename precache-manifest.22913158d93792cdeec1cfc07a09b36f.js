3f784e1851adaf24f2f293db67da5e43466ddaae
khali70 <k7alifa.a7med70@gmail.com>
2020-07-18 06:57:48 +0200

    Updates

---
+++ precache-manifest.22913158d93792cdeec1cfc07a09b36f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dd53ad9881d624a054357311a98929",
+    "url": "/E-commerce/index.html"
+  },
+  {
+    "revision": "941d528c743e92b84378",
+    "url": "/E-commerce/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ceee663b742bd43ca778",
+    "url": "/E-commerce/static/css/main.845f0445.chunk.css"
+  },
+  {
+    "revision": "941d528c743e92b84378",
+    "url": "/E-commerce/static/js/2.d9c68cb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/E-commerce/static/js/2.d9c68cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceee663b742bd43ca778",
+    "url": "/E-commerce/static/js/main.f48565ce.chunk.js"
+  },
+  {
+    "revision": "1fa0b5ccef976c535b72",
+    "url": "/E-commerce/static/js/runtime-main.c8d8c906.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/E-commerce/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/E-commerce/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/E-commerce/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/E-commerce/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/E-commerce/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/E-commerce/static/media/logo.9f7e506d.svg"
+  }
+]);
